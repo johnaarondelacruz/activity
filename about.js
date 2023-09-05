5bcb9553d29c61d452efa8abaf11bd6f5d01cef8
ikecabrido <russcabz28@gmail.com>
2023-09-05 16:06:04 +0800

    added about page

---
+++ about.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector("#menu");
+const header = document.querySelector("header");
+menu.addEventListener("click", function () {
+  header.classList.toggle("open");
+});
